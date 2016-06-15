2ba365f8c01231160c2b357d9092820491aa82b0
aaochoa <aaochoa@utp.edu.co>
2016-06-15 17:22:47 -0500

    Minor changes

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -39,15 +39,7 @@ angular.module('UTPnews', ['ui.router'])
 function($scope, posts){
 	
 	$scope.posts = posts.posts;
-	/*
-	$scope.posts = [
-	  {title: 'post 1', upvotes: 5},
-	  {title: 'post 2', upvotes: 2},
-	  {title: 'post 3', upvotes: 15},
-	  {title: 'post 4', upvotes: 9},
-	  {title: 'post 5', upvotes: 4},
-	];
-	*/
+	
 	$scope.addPost = function() {
 		if ($scope.title === '') { return; }
 		posts.create({
